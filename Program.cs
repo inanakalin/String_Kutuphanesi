ab3092258b65e109a1b1208f5f264326d0e2b4a1
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-10-03 06:47:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string degisken = "Dersimiz Csharp. Hoşgeldiniz:";
+
+        //Lenght
+        System.Console.WriteLine(degisken.Length);
+        
+        //ToUpper ToLower
+        System.Console.WriteLine(degisken.ToUpper());
+        System.Console.WriteLine(degisken.ToLower());
+
+        //Concat
+        System.Console.WriteLine(string.Concat(degisken,"Merhaba"));
+
+
+
+
+
+    }
+}
